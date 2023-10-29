d1ae3da085b6efe90aebef5064c3037a6e499043
Shakib Absar <sabsar42@gmail.com>
2023-10-29 22:03:37 +0600

    Delete CH-1_(Variables, Constants & Keywords) directory

--- CH-1_(Variables, Constants & Keywords)/C_F.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    float celsius = 37, far;
-    far = (celsius * 9 / 5) + 32;
-    printf("the value of c to f is %f", far ); 
-   
-    return 0;
-} 
--- CH-1_(Variables, Constants & Keywords)/interest-calc.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int principal,rate,years;
-    int simpleInterest = principal * rate * years;
-    printf("your interest is %d", simpleInterest);
-    return 0;
-}
