cdeb9f1d642f3befbe600bb744511c166cd2e013
Shakib Absar <sabsar42@gmail.com>
2023-10-29 22:03:27 +0600

    Delete CH-10_(File Input & Output) directory

--- CH-10_(File Input & Output)/filebasic.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    FILE  *ptr;
-    // ptr = fopen("sample.txt", "r"); //for reading a file
-    ptr = fopen("sample.txt", "w") ; // for writing  a file
-    return 0;
-}
